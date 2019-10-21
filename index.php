f21f4e9bec844b0aa2ddc52f772053c1468b51dc
9050_y_sumi <9050_y_sumi@y-sumi-mac.local>
2019-10-21 16:57:12 +0900

    creating login window...

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>リアルタイムチャット</title>
 	<link rel="stylesheet" type="text/css" href="ress.css">
-	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
 
     <?php
 	    ini_set('display_errors', 1);
@@ -30,18 +30,30 @@
 
 </head>
 <body>
-	<form method="post" action="index.php">
-	    <textarea name="message"></textarea>
-	    <button type="submit" name="sendMessage">発言</button>
-	</form>
 
-    <?php
-        $test = $db->prepare('SELECT user_name FROM users;');
-        $test->execute();
-        $test_arr = $test->fetch();
-        echo $test_arr[0].': ';
-	    echo $_POST['message'];
-	?>
+	<section class="loginWindow">
+		<form>
+			<input type="text" name="id" class="shadow">
+			<input type="password" name="pw" class="shadow">
+			<input type="submit" name="login" value="ログイン">
+		</form>
+	</section>
+
+	<section class="chatRoom">
+		<form method="post" action="index.php">
+		    <textarea name="message"></textarea>
+		    <button type="submit" name="sendMessage">発言</button>
+		</form>
+
+	    <?php
+	        $test = $db->prepare('SELECT user_name FROM users;');
+	        $test->execute();
+	        $test_arr = $test->fetch();
+	        echo $test_arr[0].': ';
+		    echo $_POST['message'];
+		?>
+
+	</section>
 
 	<script type="text/javascript" src="index.js"></script>
 </body>
